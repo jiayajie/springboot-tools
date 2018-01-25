06f76e09a5be128f16b3886959ca2a85f8de9f4a
guoxiaojing <835662306@qq.com>
2018-01-25 13:06:24 +0800

    qasd

--- src/main/java/com/example/baseproject/BaseprojectApplication.java
+++ src/main/java/com/example/baseproject/BaseprojectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseprojectApplication {
+public class BaseprojectApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaseprojectApplication.class, args);
