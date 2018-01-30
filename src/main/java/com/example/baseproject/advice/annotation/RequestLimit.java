5e602d8088202b035c675ac5850ec4d0c3119a41
dongyaofeng <dongyaofeng>
2018-01-30 17:21:39 +0800

    删除空格

--- src/main/java/com/example/baseproject/advice/annotation/RequestLimit.java
+++ src/main/java/com/example/baseproject/advice/annotation/RequestLimit.java
@@ -20,13 +20,11 @@ import java.lang.annotation.*;
 @Documented
 public @interface RequestLimit {
     /**
-     *
      * 允许访问的次数，默认值MAX_VALUE
      */
     int count() default Integer.MAX_VALUE;
 
     /**
-     *
      * 时间段，单位为毫秒，默认值一分钟
      */
     long time() default 60000;
