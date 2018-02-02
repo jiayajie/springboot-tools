b9ffd83d1de3c645032194a389fda9f661e828db
dongyaofeng <dongyaofeng>
2018-02-02 17:35:00 +0800

    删除空格

--- src/main/java/com/example/baseproject/BaseprojectApplication.java
+++ src/main/java/com/example/baseproject/BaseprojectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+//start
 @SpringBootApplication
 public class BaseprojectApplication  extends SpringBootServletInitializer{
 
