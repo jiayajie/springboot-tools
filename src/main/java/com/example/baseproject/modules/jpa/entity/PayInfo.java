8aeeb23f9a5d17df3bfb038f5abb97ba59877d9a
dongyaofeng <417212707qq.com>
2018-02-10 21:28:38 +0800

    添加 支付宝 支付

---
+++ src/main/java/com/example/baseproject/modules/jpa/entity/PayInfo.java
@@ -0,0 +1,129 @@
+package com.example.baseproject.modules.jpa.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ *  支付信息 表
+ */
+@Entity
+@Table(name = "bp_pay_info")
+public class PayInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户id
+     */
+    private Integer userId;
+
+    /**
+     * 订单号
+     */
+    private Long orderNo;
+
+    /**
+     * 支付平台:1-支付宝,2-微信
+     */
+    private Integer payPlatform;
+
+    /**
+     * 支付宝支付流水号
+     */
+    private String platformNumber;
+
+    /**
+     * 支付宝支付状态
+     */
+    private String platformStatus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    public PayInfo(Integer id, Integer userId, Long orderNo, Integer payPlatform, String platformNumber, String platformStatus, Date createTime, Date updateTime) {
+        this.id = id;
+        this.userId = userId;
+        this.orderNo = orderNo;
+        this.payPlatform = payPlatform;
+        this.platformNumber = platformNumber;
+        this.platformStatus = platformStatus;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public PayInfo() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public Integer getPayPlatform() {
+        return payPlatform;
+    }
+
+    public void setPayPlatform(Integer payPlatform) {
+        this.payPlatform = payPlatform;
+    }
+
+    public String getPlatformNumber() {
+        return platformNumber;
+    }
+
+    public void setPlatformNumber(String platformNumber) {
+        this.platformNumber = platformNumber == null ? null : platformNumber.trim();
+    }
+
+    public String getPlatformStatus() {
+        return platformStatus;
+    }
+
+    public void setPlatformStatus(String platformStatus) {
+        this.platformStatus = platformStatus == null ? null : platformStatus.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
