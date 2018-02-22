933f7ede539fa148c5532354523440cda64c75e6
dongyaofeng <dongyaofeng>
2018-02-22 09:34:49 +0800

    sso

---
+++ src/main/java/com/example/baseproject/modules/redis/controller/SessionSharingController.java
@@ -0,0 +1,15 @@
+package com.example.baseproject.modules.redis.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created With IDEA.
+ * Session 共享的 Controller
+ *  单点登录解决方案
+ * @author dongyaofeng
+ * @date 2018/2/9 9:49
+ */
+@Controller
+public class SessionSharingController {
+
+}
